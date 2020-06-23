de6eb744d79ef52c74dc07a3ec271e5910f79270
Alexey Uimanov <s9gf4ult@gmail.com>
2020-06-23 12:46:17 +0500

    Cleanup sso (#4648)
    Clean sso api packages from business logic and TH

---
+++ src/Data/Aeson/Inflections.hs
@@ -0,0 +1,52 @@
+module Data.Aeson.Inflections where
+
+import           Control.Lens
+import           Control.Monad
+import           Data.Char
+import           Data.Text as T (Text, pack, unpack)
+import           Data.Void
+import qualified Text.Inflections as I
+import           Text.Megaparsec.Error
+
+
+-- | Drops until first uppercase character and apply 'toUnderscore' to
+-- the rest
+defaultFieldLabelModifier :: String -> String
+defaultFieldLabelModifier = toUnderscore . dropPrefix
+
+defaultFieldLabelModifier' :: Text -> Text
+defaultFieldLabelModifier' = T.pack . defaultFieldLabelModifier . T.unpack
+
+-- | Just drops prefix
+dropPrefix :: String -> String
+dropPrefix = dropWhile (not . isUpper)
+
+-- | Cuts the prefix or throws error if prefix does not match. For TH
+-- modifiers only
+cutPrefix :: String -> String -> String
+cutPrefix pref s = if pref == take plen s
+  then drop plen s
+  else error "Prefix does not match!"
+  where
+    plen = length pref
+
+-- | Drops prefix and turns first character to lowercase
+dropPrefixLower :: String -> String
+dropPrefixLower = over _head toLower . dropPrefix
+
+toUnderscore :: String -> String
+toUnderscore = fromInflection I.toUnderscore
+
+toDashed :: String -> String
+toDashed = fromInflection I.toDashed
+
+fromInflection
+  :: (Text -> Either (ParseErrorBundle Text Void) Text)
+  -> String
+  -> String
+fromInflection f = either error' T.unpack . f . T.pack
+  where
+    error' = error . show . I.InflectionParsingFailed
+
+toSpaced :: String -> String
+toSpaced = fromInflection (I.toHumanized False <=< I.toUnderscore)
